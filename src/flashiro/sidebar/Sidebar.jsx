e000ff86d4a2f5fdd29245dc14bae9e356c8be3c
James Pietras <james.m.pietras@gmail.com>
2017-06-05 02:52:24 +0200

    Sidebar styles

--- src/flashiro/sidebar/Sidebar.jsx
+++ src/flashiro/sidebar/Sidebar.jsx
@@ -1,14 +1,34 @@
+import _map from 'lodash/map';
+import FontAwesome from 'react-fontawesome';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './Sidebar.scss';
 
+const menuEntries = [
+  { icon: 'home', link: '/', title: 'Dashboard' },
+  { icon: 'diamond', link: '/test', title: 'Test' },
+];
+
 const Sidebar = () => (
   <div className="sidebar">
-    <ul className="list-unstyled">
-      <li><Link to="/">Dashboard</Link></li>
-      <li><Link to="/test">Test</Link></li>
-    </ul>
+    <div className="sidebar__user-info">
+      <div className="sidebar__user-avatar" />
+      <p className="sidebar__user-name">James Pietras</p>
+    </div>
+
+    <nav>
+      <ul className="list-unstyled">
+        {_map(menuEntries, entry => (
+          <li className="sidebar__entry">
+            <Link className="sidebar__entry-link" to={entry.link}>
+              <FontAwesome className="sidebar__entry-icon" name={entry.icon} />
+              {entry.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   </div>
 );
 
