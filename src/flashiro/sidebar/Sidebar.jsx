5e139b2ae42156eed9db4cd19b7d76762a71d9b5
James Pietras <james.m.pietras@gmail.com>
2017-06-07 00:15:14 +0200

    Sidebar finishing touches

--- src/flashiro/sidebar/Sidebar.jsx
+++ src/flashiro/sidebar/Sidebar.jsx
@@ -1,35 +1,71 @@
 import _map from 'lodash/map';
+import cx from 'classnames';
 import FontAwesome from 'react-fontawesome';
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import './Sidebar.scss';
 
 const menuEntries = [
   { icon: 'home', link: '/', title: 'Dashboard' },
-  { icon: 'diamond', link: '/test', title: 'Test' },
+  { icon: 'bolt', link: '/test', title: 'Test' },
 ];
 
-const Sidebar = () => (
-  <div className="sidebar">
-    <div className="sidebar__user-info">
-      <div className="sidebar__user-avatar" />
-      <p className="sidebar__user-name">James Pietras</p>
-    </div>
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
 
-    <nav>
-      <ul className="list-unstyled">
-        {_map(menuEntries, entry => (
-          <li className="sidebar__entry">
-            <Link className="sidebar__entry-link" to={entry.link}>
-              <FontAwesome className="sidebar__entry-icon" name={entry.icon} />
-              {entry.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  </div>
-);
+    this.state = {
+      open: false,
+    };
+
+    this.toggleVisibility = this.toggleVisibility.bind(this);
+  }
+
+  toggleVisibility() {
+    this.setState({
+      open: !this.state.open,
+    });
+  }
+
+  render() {
+    return (
+      <div
+        className={cx({
+          sidebar: true,
+          'sidebar--open': this.state.open,
+        })}
+      >
+        <button className="sidebar__handle" onClick={this.toggleVisibility}>
+          <FontAwesome className="sidebar__handle-icon" name="arrow-right" />
+        </button>
+
+        <div className="sidebar__content">
+          <div className="sidebar__user-info">
+            <div className="sidebar__user-avatar" />
+            <p className="sidebar__user-name">James Pietras</p>
+          </div>
+
+          <nav>
+            <ul className="list-unstyled">
+              {_map(menuEntries, (entry, index) => (
+                <li key={index} className="sidebar__entry">
+                  <Link
+                    className="sidebar__entry-link"
+                    to={entry.link}
+                    onClick={this.toggleVisibility}
+                  >
+                    <FontAwesome className="sidebar__entry-icon" name={entry.icon} />
+                    {entry.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Sidebar;
